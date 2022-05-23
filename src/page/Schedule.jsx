2da07a4095bd19bf3738f6eb701cdb560a148e26
it62070120 <62070120@it.kmitl.ac.th>
2022-05-24 00:56:38 +0700

    build: delete schedule

--- src/page/Schedule.jsx
+++ src/page/Schedule.jsx
@@ -2,94 +2,92 @@ import { Button, ModalHeader, ModalBody, ModalDialog, ModalFooter, Table, ModalT
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from 'react';
 import Subject from './../components/Subject';
-import { useParams, useLocation } from "react-router-dom"
+import { useParams, useLocation, useNavigate } from "react-router-dom"
 import Footer from '../components/Footer';
 import axios from "../plugins/axios";
 
 const Schedule = () => {
   const location = useLocation()
   const { subjects } = location.state
-
+  const schedule_id = subjects
+  console.log(schedule_id)
+  const navigate = useNavigate();
   
   const [subjectData, setSubjectData] = useState(null)
+  const [deleteStatus, setDeleteStatus] = useState("hidden")
 
-    async function getScheduleList(){
-        let response = await axios.get(`/getsubjectbysid/${subjects}`)
-        console.log(response.data)
-        let scheduleList = response.data
-        setSubjectData(scheduleList)
-        console.log(subjectData)
-      }
-      useEffect(() => {    // Update the document title using the browser API    
-        
-        getScheduleList();
-      }, []);
-  // const [subjects, setSubjects] = useState([
-  //   {
-  //     id: "1",
-  //     name: "Math",
-  //     day: "Monday",
-  //     time: "13.00-16.00",
-  //     link: "www.math.com"
-  //   },
-  //   {
-  //     id: "2",
-  //     name: "Human interface developer",
-  //     day: "Wednesday",
-  //     time: "9.00-12.00",
-  //     link: "www.Humaninterfacedeveloper.com"
-  //   },
-  //   {
-  //     id: "3",
-  //     name: "Software development tools",
-  //     day: "Friday",
-  //     time: "13.00-16.00",
-  //     link: "www.Softwaredevelopmenttools.com"
-  //   },
-  // ]);
+  // Get Schedules
+  async function getScheduleList(){
+    let response = await axios.get(`/getsubjectbysid/${schedule_id}`)
+    console.log(response.data)
+    let scheduleList = response.data
+    setSubjectData(scheduleList)
+    console.log(subjectData)
+  }
 
-  // axios
+  useEffect(() => { // Update the document title using the browser API
+    getScheduleList();
+  }, []);
 
-    return(
-        <div className="App">
-          <div className="container mt-5">
-            <Table striped bordered hover className='mt-5'>
-                <thead>
-                  <tr>
-                    <th>Day</th>
-                    <th>Subject</th>
-                    <th>Time</th>
-                    <th>Link</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  { subjectData && subjectData.map((subject) => {
-                    return (
-                      <Subject key={subject.id} subject={subject} />
-                    )
-                  }) }
-                </tbody>
-              </Table>
-              <Button variant="danger" style={{width: "100%"}} onclick="">Delete this schedule</Button>
-              <ModalDialog style={{visibility: "show"}}>
-                <ModalHeader closeButton>
-                  <ModalTitle>Confirm to delete this schedule.</ModalTitle>
-                </ModalHeader>
+  const handleDeleteButton = (status) => {
+    setDeleteStatus(status);
+  }
 
-                <ModalBody>
-                  <p>Please choose your choice.</p>
-                </ModalBody>
+  // Delete Schedule
+  async function handleDeleteSubmit() {
+    console.log(schedule_id)
+    await axios.delete(`/subject/delbyscid`, {data: {schedule_id: schedule_id}})
+    .catch ((err) => {
+      console.log(err);
+    });
+    await axios.delete('/deleteschedule', { data: {schedule_id: schedule_id }})
+    .then ((res) => {
+      alert(res.data)
+      navigate('/')
+    }).catch ((err) => {
+      console.log(err);
+    });
+  }
 
-                <ModalFooter>
-                  <Button variant="secondary">Close</Button>
-                  <Button variant="primary">Delete</Button>
-                </ModalFooter>
-            </ModalDialog>
-          </div>
-          <Footer/>
+  return(
+    <div className="App">
+      <div className="container mt-5">
+        <Table striped bordered hover className='mt-5'>
+            <thead>
+              <tr>
+                <th>Day</th>
+                <th>Subject</th>
+                <th>Time</th>
+                <th>Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              { subjectData && subjectData.map((subject) => {
+                return (
+                  <Subject key={subject.id} subject={subject} />
+                )
+              }) }
+            </tbody>
+          </Table>
+          <Button variant="danger" style={{width: "100%"}} onClick={() => handleDeleteButton("visible")}>Delete this schedule</Button>
+          <ModalDialog style={{visibility: deleteStatus}}>
+            <ModalHeader closeButton>
+              <ModalTitle>Confirm to delete this schedule.</ModalTitle>
+            </ModalHeader>
+
+            <ModalBody>
+              <p>Please choose your choice.</p>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button variant="secondary" onClick={() => handleDeleteButton("hidden")}>Close</Button>
+              <Button variant="primary" onClick={handleDeleteSubmit}>Delete</Button>
+            </ModalFooter>
+        </ModalDialog>
       </div>
-
-    )
+      <Footer/>
+    </div>
+  )
 }
 
 export default Schedule
